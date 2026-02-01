cedd13972e265d6cbcec095237e733f539d37ada
jharrvis <jharrvis.forwarded@gmail.com>
2026-02-02 01:36:07 +0700

    Remove Sanctum dependency - not needed for webhook

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,9 @@ use App\Http\Controllers\CekatWebhookController;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| Here is where you can register API routes for your application.
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 // Cekat Webhook Route
 Route::post('/webhook/cekat', [CekatWebhookController::class, 'handle']);
